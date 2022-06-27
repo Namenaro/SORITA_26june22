8591fc7c1386073ce7a403df5c12d6f704f37512
namenaro <nanenaro@gmail.com>
2022-06-27 17:57:55 +0300

    start work on first collaterals для простейшей сигны

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from utils.get_pictures import get_omni_pics
 from utils.point import Point
 from proofer_by_sampler import Proofer
 from situation_finder import SituationFinderInVicinity, SituationFinderRandom
+from prediction import *
+from stat_hypo import *
+
 import random
-import numpy as np
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -55,6 +55,9 @@ def sense_0(point, picture): #hist on omni pic ={255: 0.9264399092970521, 0: 0.0
 def runA(point):
     return sense_0(point, pic)
 
+def empty_run(point):
+    return True
+
 def collateral_1A(point):
     new_point = Point(x=point.x+1, y=point.y)
     return runA(new_point)
@@ -78,7 +81,35 @@ def check_possible_collateral():
     sit_finder = SituationFinderRandom(pic, run_condition)
     proofer = Proofer(pic,  run_exp, actual_p_for_success_of_exp, sit_finder, max_attempts=20)
     success, p_val, sample= proofer.try_sample()
-    print ("cheked: " + str(success) + ', p_val = '+ str(p_val)+ ", sanmple size=" + str(len(sample)))
+    print ("pair condition-collateral cheked: " + str(success) + ', p_val = '+ str(p_val)+ ", sample size=" + str(len(sample)))
+
+
+    sit_finder = SituationFinderRandom(pic, run_condition)
+    pred_sampler = PredictionSampler(pic, collateral_1A, sit_finder)
+    pred_sampler.fill_sample(250)
+    print("p(1)=" + str(pred_sampler.get_p_of_one()))
+    pred_sampler.show_hist()
+
+def test_sample_against_sample():
+    NO_CONDITION_SIZE=60
+    CONDITION_SIZE=3
+
+    sit_finder1 = SituationFinderRandom(pic, empty_run)
+    pred_sampler1 = PredictionSampler(pic, collateral_1A, sit_finder1)
+    pred_sampler1.fill_sample(NO_CONDITION_SIZE)
+    print("p(1)=" + str(pred_sampler1.get_p_of_one()))
+
+    sit_finder2 = SituationFinderRandom(pic, runA)
+    pred_sampler2 = PredictionSampler(pic, collateral_1A, sit_finder2)
+    pred_sampler2.fill_sample(CONDITION_SIZE)
+    print("p(1|1->)=" + str(pred_sampler1.get_p_of_one()))
+
+    pval=get_p_value_for_two_samples(pred_sampler1.sample, pred_sampler2.sample)
+    print("two samples PVAL=" + str(pval))
+
+
+
+
 
 def start_info():
     hist = get_hist_normed(x)
@@ -87,6 +118,6 @@ def start_info():
     show_hist(hist, ax)
     plt.show()
 
-check_possible_collateral()
-
+#check_possible_collateral()
+test_sample_against_sample()
 
---
+++ prediction.py
@@ -0,0 +1,53 @@
+from situation_finder import *
+import matplotlib.pyplot as plt
+
+class PredictionSampler:
+    def __init__(self, pic, run_collateral, sit_finder):
+        self.pic=pic
+        self.run_collateral=run_collateral
+        self.sit_finder=sit_finder
+        self.sample = []
+
+
+    def fill_sample(self, attemts=20):
+        for _ in range(attemts):
+            point = self.sit_finder.get_next_uncheked_situation()
+            outcome = self.run_collateral(point)
+            if outcome is not None:
+                self.sample.append(outcome)
+
+    def get_hist_unnormed(self):
+        hist = {}
+        for outcome in self.sample:
+            hist[outcome] = hist.get(outcome, 0) + 1
+        return hist
+
+    def get_hist_normed(self):
+        hist = self.get_hist_unnormed()
+        num_elements = 0
+        for key in hist.keys():
+            num_elements += hist[key]
+        for key in hist.keys():
+            hist[key] = hist[key] / num_elements
+        return hist
+
+    def show_hist(self):
+        fig, ax = plt.subplots()
+        hist = self.get_hist_normed()
+        ps = []
+        keys = []
+        for key in hist.keys():
+            keys.append(str(key))
+            ps.append(hist[key])
+        ax.bar(keys, ps, color='#774444', edgecolor="k", linewidth=2)
+        ax.set_ylabel('p')
+        ax.set_ylim([0, 1])
+        ax.set_title('Histogram')
+        plt.show()
+
+    def get_p_of_one(self):
+        hist = self.get_hist_normed()
+        return hist[True]
+
+
+
